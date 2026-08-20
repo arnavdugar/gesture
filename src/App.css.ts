8b54ed7badb994042544549e983e945466793211
Arnav Dugar <arnavdugar@gmail.com>
2026-08-19 17:36:41 -0700

    Implement audio

--- src/App.css.ts
+++ src/App.css.ts
@@ -1,10 +1,27 @@
 import { style } from "@vanilla-extract/css";
 
-const fullScreenLayer = style({
+export const menu = style({
+  alignItems: "center",
+  backdropFilter: "blur(8px)",
+  backgroundColor: "rgba(255, 255, 255, 0.1)",
+  border: "1px solid rgba(255, 255, 255, 0.25)",
+  borderRadius: 8,
+  bottom: 16,
+  display: "flex",
+  gap: 16,
+  minHeight: 48,
+  justifyContent: "start",
+  left: 16,
+  padding: "4px 12px",
   position: "absolute",
-  inset: 0,
-  width: "100%",
+  right: 16,
+});
+
+const fullScreenLayer = style({
   height: "100%",
+  inset: 0,
+  position: "absolute",
+  width: "100%",
 });
 
 export const video = style([
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,52 @@
+import { useState } from "preact/hooks";
+
 import * as styles from "./App.css";
+import { Dropdown } from "./components/dropdown";
 import { HandOverlay } from "./HandOverlay";
+import { useAudio, type Scale, type Waveform } from "./useAudio";
 import { useHandTracking } from "./useHandTracking";
 
+const rootOptions = [
+  { label: "A", value: -3 },
+  { label: "A♯ / B♭", value: -2 },
+  { label: "B", value: -1 },
+  { label: "C", value: 0 },
+  { label: "C♯ / D♭", value: 1 },
+  { label: "D", value: 2 },
+  { label: "D♯ / E♭", value: 3 },
+  { label: "E", value: 4 },
+  { label: "F", value: 5 },
+  { label: "F♯ / G♭", value: 6 },
+  { label: "G", value: 7 },
+  { label: "G♯ / A♭", value: 8 },
+] satisfies ReadonlyArray<{ label: string; value: number }>;
+
+const scaleOptions = [
+  { label: "Major", value: "major" },
+  { label: "Minor", value: "minor" },
+  { label: "Dorian", value: "dorian" },
+  { label: "Phrygian", value: "phrygian" },
+  { label: "Lydian", value: "lydian" },
+  { label: "Mixolydian", value: "mixolydian" },
+  { label: "Locrian", value: "locrian" },
+] satisfies ReadonlyArray<{ label: string; value: Scale }>;
+
+const waveformOptions = [
+  { label: "Sawtooth", value: "sawtooth" },
+  { label: "Sine", value: "sine" },
+  { label: "Square", value: "square" },
+  { label: "Triangle", value: "triangle" },
+  { label: "Organ", value: "organ" },
+  { label: "Pulse", value: "pulse" },
+  { label: "Shimmer", value: "shimmer" },
+] satisfies ReadonlyArray<{ label: string; value: Waveform }>;
+
 export function App() {
+  const [root, setRoot] = useState<number>(0);
+  const [scale, setScale] = useState<Scale>("major");
+  const [waveform, setWaveform] = useState<Waveform>("sawtooth");
   const { videoRef, videoWidth, videoHeight, data } = useHandTracking();
+  useAudio(data, root, scale, waveform);
 
   return (
     <>
@@ -13,6 +56,28 @@ export function App() {
         videoHeight={videoHeight}
         data={data}
       />
+      <div class={styles.menu}>
+        <Dropdown
+          label="Root:"
+          options={rootOptions}
+          value={root}
+          onChange={(event) => setRoot(parseInt(event.currentTarget.value))}
+        />
+        <Dropdown
+          label="Scale:"
+          options={scaleOptions}
+          value={scale}
+          onChange={(event) => setScale(event.currentTarget.value as Scale)}
+        />
+        <Dropdown
+          label="Waveform:"
+          options={waveformOptions}
+          value={waveform}
+          onChange={(event) =>
+            setWaveform(event.currentTarget.value as Waveform)
+          }
+        />
+      </div>
     </>
   );
 }
